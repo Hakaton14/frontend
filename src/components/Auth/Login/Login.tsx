5cd348d91bde55d8c135763aa4f8a10e0ba67aaa
Maxim Talanov <mail@maxtalanov.ru>
2023-10-24 19:57:42 +0300

    feat(login): Сделать валидацию формы

--- src/components/Auth/Login/Login.tsx
+++ src/components/Auth/Login/Login.tsx
@@ -1,20 +1,54 @@
 import { FC } from "react"
+import { useForm } from "react-hook-form"
+import { yupResolver } from "@hookform/resolvers/yup"
 import { Button } from "@mui/material"
 import { Input } from "@UI"
+import { loginShema } from "@Utils"
 import styles from "./Login.module.scss"
 
+type formLogin = {
+  email: string
+  password: string
+}
+
 const Login: FC = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(loginShema),
+  })
+
+  const onSubmit = (data: any) => {
+    console.log(data, "Данные авторизации")
+  }
+
   return (
     <div className={styles.container}>
       <h2 className={styles.title}>Войти в аккаунт</h2>
 
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)} noValidate>
         <div className={styles.input}>
-          <Input type={"email"} placeholder={"Почта"} />
+          <Input
+            type="email"
+            placeholder={"Почта"}
+            register={register}
+            registerName={"email"}
+            error={!!errors.email}
+            helperText={errors.email?.message}
+          />
         </div>
 
         <div className={styles.input}>
-          <Input type={"password"} placeholder={"Пароль"} />
+          <Input
+            type="password"
+            placeholder={"Пароль"}
+            register={register}
+            registerName={"password"}
+            error={!!errors.password}
+            helperText={errors.password?.message}
+          />
         </div>
 
         <div className={styles.button}>
@@ -23,11 +57,11 @@ const Login: FC = () => {
           </Button>
         </div>
 
-        <div className={styles.button}>
+        {/* <div className={styles.button}>
           <Button type="submit" fullWidth variant="outlined" size="medium">
             Войти с Яндекс ID
           </Button>
-        </div>
+        </div> */}
       </form>
 
       <div>
