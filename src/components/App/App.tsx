062f7064c915e2690505b6e44e15d58d5e083901
Gleb <113468160+Kaygorodcevg@users.noreply.github.com>
2023-10-24 21:16:46 +0500

    Создать роутинг, добавить header

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,18 @@
+import { Routes, Route } from "react-router-dom"
 import { Auth } from "@Components"
+import Registration from "../Auth/Registration/Registration"
+import MainScreen from "../MainScreen/MainScreen"
 
 function App() {
   return (
-    <div className="App">
-      <Auth />
-    </div>
+    // <div className="App">
+    //   <Auth />
+    // </div>
+    <Routes>
+      <Route path="sign-in" element={<Auth />}></Route>
+      <Route path="sign-up" element={<Registration />}></Route>
+      <Route path="main" element={<MainScreen />}></Route>
+    </Routes>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import { Provider } from "react-redux"
+import { BrowserRouter } from "react-router-dom"
 import { store } from "./app/store"
 import { App } from "@Components"
 import "./index.css"
@@ -8,7 +9,9 @@ import "./index.css"
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
 )
