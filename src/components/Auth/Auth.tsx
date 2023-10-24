8f41db8225a3459f33809063d2f216a374ce71f8
Maxim Talanov <mail@maxtalanov.ru>
2023-10-24 15:55:45 +0300

    refactor(auth): Скорректировать компонент auth
    - Типизировать пропсы
    - Переделать компонент на FC
    - Добавить динамическую отрисовку подкомпонента (Дефолтное значение: Login)

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -1,12 +1,25 @@
+import { FC } from "react"
 import styles from "./Auth.module.scss"
 import Login from "./Login/Login"
+import Registration from "./Registration/Registration"
 
-function Auth() {
+enum EPodComponent {
+  login,
+  registration,
+}
+
+type TPodComponent = keyof typeof EPodComponent
+
+interface IAuthProps {
+  podComponent: TPodComponent
+}
+
+const Auth: FC<IAuthProps> = ({ podComponent = "login" }) => {
   return (
     <section className={styles.wrapper}>
       <div className={styles.container}>
         <h1 className={styles.title}>Карьерный трекер</h1>
-        <Login />
+        {podComponent === "login" ? <Login /> : <Registration />}
       </div>
     </section>
   )
