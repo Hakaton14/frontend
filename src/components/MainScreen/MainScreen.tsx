bbc93d23afd841f2c87520a928c90c5121095955
Vitkor <vitland@gmail.com>
2023-10-26 17:48:06 +0300

    refactor: Добавить попап на главную страницу

--- src/components/MainScreen/MainScreen.tsx
+++ src/components/MainScreen/MainScreen.tsx
@@ -1,11 +1,13 @@
 import { FC } from "react"
 import styles from "./MainScreen.module.scss"
 import Header from "../Header/Header"
+import { VacancyPopup } from "@Components"
 
 const MainScreen: FC = () => {
   return (
     <div className={styles.container}>
       <Header />
+      <VacancyPopup openPopup={true}/>
     </div>
   )
 }
