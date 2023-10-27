cc7e44e7bc808e8e14ebd0196e205cb5ffe942d4
Maxim Talanov <mail@maxtalanov.ru>
2023-10-27 17:53:47 +0300

    feat(notification): добавлен компонент Notification в индексный файл UI

--- src/ui-kit/index.ts
+++ src/ui-kit/index.ts
@@ -1,3 +1,5 @@
 // Пример: export { default as Компонент } from "./Компонент/Компонент"
+export { default as Notification } from "./Notification/Notification"
+
 export type { TInputProps } from "./Input/Input"
 export { default as Input } from "./Input/Input"
