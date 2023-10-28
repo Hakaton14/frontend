8715cd347e5245f75674199020b941202d37102e
Maxim Talanov <mail@maxtalanov.ru>
2023-10-28 17:06:04 +0300

    feat(resume): Добавить алиас Resume Icom + добавить ф-ил индексации

---
+++ src/ui-kit/icons/resume/index.ts
@@ -0,0 +1,33 @@
+import AvatarResumeMoke from "./AvatarResumeMoke.png"
+import Behance from "./behance.svg"
+import Download from "./download.svg"
+import Email from "./email.svg"
+import EyeSlashed from "./eye-slashed.svg"
+import Eye from "./eye.svg"
+import FaceBook from "./facebook.svg"
+import CloseMini from "./closeMini.svg"
+import LikeFilled from "./Like-filled.svg"
+import Like from "./Like.svg"
+import Linkedin from "./linkedin.svg"
+import Phone from "./phone.svg"
+import Share from "./share.svg"
+import Telegram from "./telegram.svg"
+import VK from "./vk.svg"
+
+export {
+  AvatarResumeMoke,
+  Behance,
+  Download,
+  Email,
+  EyeSlashed,
+  Eye,
+  FaceBook,
+  CloseMini,
+  LikeFilled,
+  Like,
+  Linkedin,
+  Phone,
+  Share,
+  Telegram,
+  VK,
+}
