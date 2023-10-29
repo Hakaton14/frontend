7d1986a2344a10b607cb6d3289c30b5642f55c97
Maxim Talanov <mail@maxtalanov.ru>
2023-10-30 00:29:39 +0300

    feat(resume): Доработать разметку и стили

--- src/components/CandidateResume/CandidateResume.tsx
+++ src/components/CandidateResume/CandidateResume.tsx
@@ -1,16 +1,22 @@
 import { Avatar } from "@mui/material"
+import { useChangeTitle } from "@Hooks"
 import { AvatarResumeMoke } from "@IconResume"
 import { ToolBar } from "./ToolBar/ToolBar"
 import { ButtonPanel } from "./ButtonPanel/ButtonPanel"
-import styles from "./CandidateResume.module.scss"
 import { Bio } from "./Bio/Bio"
+import { BasicInfo } from "./BasicInfo/BasicInfo"
+import { Resume } from "./MokeData"
+import styles from "./CandidateResume.module.scss"
 
 const CandidateResume = () => {
+  useChangeTitle("Анкета Кандидата")
+
   return (
     <section className={styles.Container}>
       <div className={styles.ToolBar}>
         <ToolBar />
       </div>
+
       <div className={styles.ContainerTop}>
         <Avatar
           alt="Remy Sharp"
@@ -21,8 +27,17 @@ const CandidateResume = () => {
         <ButtonPanel />
         <div className={styles.Bio}></div>
       </div>
+
       <div className={styles.ContainerBottom}>
-        
+        <div className={styles.Left}>
+          <BasicInfo title="Обо мне" paragraph={Resume.about} />
+          <BasicInfo title="Навыки" paragraph={Resume.skills} />
+        </div>
+
+        <div className={styles.Right}>
+          <BasicInfo title="Опыт работы" paragraph={Resume.expertise} />
+          <BasicInfo title="Образование" paragraph={Resume.education} />
+        </div>
       </div>
     </section>
   )
