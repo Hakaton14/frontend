6517591cae0ce527155f9ccf6c5ae1f175ed47a6
Gleb <113468160+Kaygorodcevg@users.noreply.github.com>
2023-10-29 10:01:53 +0500

    Добавить фильтры на страницу поиска

---
+++ src/components/Filters/Filters.tsx
@@ -0,0 +1,12 @@
+import styles from "./Filter.module.scss"
+
+function Filters() {
+  return (
+    <>
+      <h1 className={styles.title}>Фильтры</h1>
+      <div className={styles.filterTbale}></div>
+    </>
+  )
+}
+
+export default Filters
