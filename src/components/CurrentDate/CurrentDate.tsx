e8f1a0c92f5082aca73301057a971e2d155d397e
Kikodze Viktor <vitland-x@yandex.ru>
2023-10-30 22:43:24 +0300

    fix: Часы

--- src/components/CurrentDate/CurrentDate.tsx
+++ src/components/CurrentDate/CurrentDate.tsx
@@ -2,7 +2,19 @@ import { Typography } from "@mui/material"
 import { Box } from "@mui/system"
 import dayjs from "dayjs"
 import "dayjs/locale/ru"
+import { useEffect, useState } from "react"
+
 function CurrentDate() {
+  const [value, setValue] = useState(new Date())
+
+  useEffect(() => {
+    const interval = setInterval(() => setValue(new Date()), 1000)
+
+    return () => {
+      clearInterval(interval)
+    }
+  }, [])
+
   return (
     <Box
       sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
@@ -11,7 +23,7 @@ function CurrentDate() {
         {dayjs(Date()).locale("ru").format("D MMMM YYYY")}
       </Typography>
       <Typography variant="h4">
-        {dayjs().locale("ru").format("HH:MM:ss")}
+        {dayjs(value).locale("ru").format("HH:mm")}
       </Typography>
     </Box>
   )
