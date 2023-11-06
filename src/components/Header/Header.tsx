a8ed5179f19a018b2da1a9796dcf6cde5e89fb78
Максим Таланов <mail@maxtalanov.ru>
2023-11-07 01:53:21 +0300

    refactor(header): Скорректировать разметку и стили
    - Удалить избыточную обертку
    - Перенести стили в переменную
    - Скорректировать стили

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,60 +8,65 @@ import UserIcon from "../../ui-kit/icons/user.svg"
 
 import styles from "./Header.module.scss"
 
+const styleSx = {
+  appBar: {
+    backgroundColor: "#1A1B22",
+    Width: "100%",
+  },
+}
+
 function Header() {
   const navigate = useNavigate()
   const { pathname } = useLocation()
 
   return (
-    <Box sx={{ flexGrow: 1, Width: "100%" }}>
-      <AppBar position="static" sx={{ backgroundColor: "#1A1B22" }}>
-        <Toolbar>
-          <img
-            onClick={() => navigate("/")}
-            src={Logo}
-            alt="Логотип"
-            className={styles.img}
-            style={{
-              margin: "10px 148px 10px 83px",
+    <AppBar position="fixed" sx={styleSx.appBar}>
+      <Toolbar>
+        <img
+          onClick={() => navigate("/")}
+          src={Logo}
+          alt="Логотип"
+          className={styles.img}
+          style={{
+            margin: "10px 148px 10px 83px",
+          }}
+        />
+
+        {pathname !== "/search" && (
+          <Button
+            onClick={() => navigate("/search")}
+            variant="text"
+            sx={{ color: "#FFF" }}
+          >
+            <img src={SearchIcon} alt="Иконка поиска" />
+            Поиск кандидатов
+          </Button>
+        )}
+
+        <Box sx={{ flexGrow: 1 }} />
+
+        <Box sx={{ display: { xs: "none", md: "flex" } }}>
+          <IconButton sx={{ padding: "0", marginRight: "52px" }}>
+            <img src={UserIcon} alt="Иконка пользователя" />
+          </IconButton>
+
+          <IconButton
+            sx={{
+              padding: "0",
+              marginRight: "52px",
+              width: "24px",
+              height: "24px",
             }}
-          />
+          >
+            <img src={GroupIcon} alt="Иконка писем" />
+          </IconButton>
 
-          {pathname !== "/search" && (
-            <Button
-              onClick={() => navigate("/search")}
-              variant="text"
-              sx={{ color: "#FFF" }}
-            >
-              <img src={SearchIcon} alt="Иконка поиска" />
-              Поиск кандидатов
-            </Button>
-          )}
-
-          <Box sx={{ flexGrow: 1 }} />
-
-          <Box sx={{ display: { xs: "none", md: "flex" } }}>
-            <IconButton sx={{ padding: "0", marginRight: "52px" }}>
-              <img src={UserIcon} alt="Иконка пользователя" />
-            </IconButton>
-
-            <IconButton
-              sx={{
-                padding: "0",
-                marginRight: "52px",
-                width: "24px",
-                height: "24px",
-              }}
-            >
-              <img src={GroupIcon} alt="Иконка писем" />
-            </IconButton>
-
-            <IconButton sx={{ padding: "0", width: "24px", height: "24px" }}>
-              <img src={NotificationIcon} alt="Иконка уведомлений" />
-            </IconButton>
-          </Box>
-        </Toolbar>
-      </AppBar>
-    </Box>
+          <IconButton sx={{ padding: "0", width: "24px", height: "24px" }}>
+            <img src={NotificationIcon} alt="Иконка уведомлений" />
+          </IconButton>
+        </Box>
+      </Toolbar>
+    </AppBar>
   )
 }
 
