44679d9cd0a12990464dff0b5216acc23dbdd8f0
Максим Таланов <mail@maxtalanov.ru>
2023-11-07 05:56:15 +0300

    fix(header): Исправить техническую конструкцию отключения авторизации
    - Protected router

--- src/components/ProtectedRoute/ProtectedRoute.tsx
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -2,8 +2,7 @@ import { useAppSelector } from "@ReduxHooks"
 import { Navigate } from "react-router-dom"
 
 const ProtectedRoute = ({ element: Component, ...props }: any) => {
-  // const { user } = useAppSelector((state) => state.auth)
-  const user = true
+  const { user } = useAppSelector((state) => state.auth)
 
   return user ? <Component {...props} /> : <Navigate to="/sign-in" replace />
 }
