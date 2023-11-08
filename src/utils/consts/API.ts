69c1af67413b80d6be6ff40d36c5f858ee7ee195
Максим Таланов <mail@maxtalanov.ru>
2023-11-08 15:12:57 +0300

    ..

--- src/utils/consts/API.ts
+++ src/utils/consts/API.ts
@@ -1,4 +1,7 @@
-export const API_URL = import.meta.env.VITE_API_URL
-
-// TODO: временно
-console.log(import.meta.env)
+const MODE = import.meta.env.MODE
+const args = "api"
+const version = "v1"
+export const API_URL =
+  MODE === "development"
+    ? `http://localhost:8000/${args}/${version}`
+    : `https://hr-praktikum.webtm.ru/${args}/${version}`
