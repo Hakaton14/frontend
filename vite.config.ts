d265200431979e9bbfa1d95c7847aab1ac37e99e
Максим Таланов <mail@maxtalanov.ru>
2023-11-08 07:44:20 +0300

    fix(env): Исправить ошибку переменных окружения
    - Получить доступ к файлу .env из каталога backend
    - Передать нужные переменные в окружение .env vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from "vitest/config"
 import react from "@vitejs/plugin-react"
+import { loadEnv } from "./config/envConfig"
+
+const env = loadEnv()
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    "process.env": env,
+  },
   resolve: {
     alias: {
       "@Components": "./src/components/index.ts",
