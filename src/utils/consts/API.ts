8939b91cb8b9e677506aa46bd450878a1d6eedce
Максим Таланов <55347015+maxtalanov@users.noreply.github.com>
2023-11-10 20:25:40 +0300

    Update API.ts
    Замера ошибочного urla

--- src/utils/consts/API.ts
+++ src/utils/consts/API.ts
@@ -4,4 +4,4 @@ const version = "v1"
 export const API_URL =
   MODE === "development"
     ? `http://localhost:8000/${args}/${version}`
-    : `https://hr-praktikum.webtm.ru/${args}/${version}`
+    : `https://hr-practicum.webtm.ru/${args}/${version}`
