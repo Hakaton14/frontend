511adb19a9f9ee1b223495b0745d0857789653fa
Максим Таланов <mail@maxtalanov.ru>
2023-11-16 14:10:56 +0300

    feat(candidate table): Добавить негативный сценарий при отсутствии вакансий

--- src/components/CandidateTable/CandidateTable.tsx
+++ src/components/CandidateTable/CandidateTable.tsx
@@ -8,6 +8,7 @@ import {
   LinearProgress,
   Button,
   Stack,
+  Typography,
 } from "@mui/material"
 import { ExpandMore } from "@mui/icons-material"
 import PencilIcon from "../../ui-kit/icons/pencil-1.svg"
@@ -23,108 +24,124 @@ const CandidateTable: FC = () => {
   dayjs.extend(relativeTime)
 
   const vacancies = useAppSelector((state) => state.vacancies.vacancyList)
-
+  console.log("Список вакансий", vacancies.length)
   return (
     <Stack spacing={1}>
-      {vacancies.map((vacancy) => (
-        <Accordion sx={{ width: "945px" }} key={vacancy.id}>
-          <AccordionSummary
-            sx={{
-              width: "945px",
-              height: "104px",
-              padding: "24px 32px",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-            expandIcon={<ExpandMore sx={{ width: "24px", height: "24px" }} />}
-            id="panel1a-header"
-          >
-            <div className={styles.tableWrapper}>
-              <div className={styles.professionWrapper}>
-                <h2 className={styles.vacantionTitle}>{vacancy.name}</h2>
+      {vacancies.length === 0 ? (
+        <Typography
+          sx={{
+            paddingTop: "56px",
+            fontFamily: "YS Display",
+            fontSize: "24px",
+            fontWeight: 500,
+            lineHeight: "32px",
+            letterSpacing: "0em",
+          }}
+          variant="h2"
+        >
+          Список вакансий отсутствует
+        </Typography>
+      ) : (
+        vacancies.map((vacancy) => (
+          <Accordion sx={{ width: "945px" }} key={vacancy.id}>
+            <AccordionSummary
+              sx={{
+                width: "945px",
+                height: "104px",
+                padding: "24px 32px",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+              expandIcon={<ExpandMore sx={{ width: "24px", height: "24px" }} />}
+              id="panel1a-header"
+            >
+              <div className={styles.tableWrapper}>
+                <div className={styles.professionWrapper}>
+                  <h2 className={styles.vacantionTitle}>{vacancy.name}</h2>
 
-                <h3 className={styles.progressTitle}>
-                  {dayjs(vacancy.pub_datetime).locale("ru").fromNow()}
-                </h3>
-              </div>
+                  <h3 className={styles.progressTitle}>
+                    {dayjs(vacancy.pub_datetime).locale("ru").fromNow()}
+                  </h3>
+                </div>
 
-              <div className={styles.progressWrapper}>
-                <h3 className={styles.progress}>Статус: 75%</h3>
+                <div className={styles.progressWrapper}>
+                  <h3 className={styles.progress}>Статус: 75%</h3>
 
-                <LinearProgress
-                  sx={{ width: "132px", height: "8px", borderRadius: "64px" }}
-                  variant="determinate"
-                  value={75}
-                />
-              </div>
-
-              {/* TODO: Вынести в подкомпонент  */}
-              <div className={styles.buttonWrapper}>
-                <Button
-                  sx={{
-                    padding: "0",
-                    paddingRight: "24px",
-                    fontSize: "14px",
-                    lineHeight: "20px",
-                    fontWeight: "400",
-                    color: "#5A9BFF",
-                    textTransform: "none",
-                  }}
-                >
-                  <img
-                    className={styles.icon}
-                    src={PencilIcon}
-                    alt="Иконка карандаша"
+                  <LinearProgress
+                    sx={{ width: "132px", height: "8px", borderRadius: "64px" }}
+                    variant="determinate"
+                    value={75}
                   />
-                  Изменить
-                </Button>
+                </div>
 
-                <Button
-                  sx={{
-                    padding: "0",
-                    paddingRight: "24px",
-                    fontSize: "14px",
-                    lineHeight: "20px",
-                    fontWeight: "400",
-                    color: "#5A9BFF",
-                    textTransform: "none",
-                  }}
-                >
-                  <img
-                    className={styles.icon}
-                    src={FolderIcon}
-                    alt="Иконка папки"
-                  />
-                  В архив
-                </Button>
+                {/* TODO: Вынести в подкомпонент  */}
+                <div className={styles.buttonWrapper}>
+                  <Button
+                    sx={{
+                      padding: "0",
+                      paddingRight: "24px",
+                      fontSize: "14px",
+                      lineHeight: "20px",
+                      fontWeight: "400",
+                      color: "#5A9BFF",
+                      textTransform: "none",
+                    }}
+                  >
+                    <img
+                      className={styles.icon}
+                      src={PencilIcon}
+                      alt="Иконка карандаша"
+                    />
+                    Изменить
+                  </Button>
 
-                <Button
-                  sx={{
-                    padding: "0",
-                    fontSize: "14px",
-                    lineHeight: "20px",
-                    fontWeight: "400",
-                    color: "#5A9BFF",
-                    textTransform: "none",
-                  }}
-                >
-                  <img
-                    className={styles.icon}
-                    src={EyeIcon}
-                    alt="Иконка глаза"
-                  />
-                  Скрыть
-                </Button>
+                  <Button
+                    sx={{
+                      padding: "0",
+                      paddingRight: "24px",
+                      fontSize: "14px",
+                      lineHeight: "20px",
+                      fontWeight: "400",
+                      color: "#5A9BFF",
+                      textTransform: "none",
+                    }}
+                  >
+                    <img
+                      className={styles.icon}
+                      src={FolderIcon}
+                      alt="Иконка папки"
+                    />
+                    В архив
+                  </Button>
+
+                  <Button
+                    sx={{
+                      padding: "0",
+                      fontSize: "14px",
+                      lineHeight: "20px",
+                      fontWeight: "400",
+                      color: "#5A9BFF",
+                      textTransform: "none",
+                    }}
+                  >
+                    <img
+                      className={styles.icon}
+                      src={EyeIcon}
+                      alt="Иконка глаза"
+                    />
+                    Скрыть
+                  </Button>
+                </div>
               </div>
-            </div>
-          </AccordionSummary>
+            </AccordionSummary>
 
-          <AccordionDetails sx={{ padding: "24px 24px" }}>
-            <CandidatInfoTable />
-          </AccordionDetails>
-        </Accordion>
-      ))}
+            <AccordionDetails sx={{ padding: "24px 24px" }}>
+              <CandidatInfoTable />
+            </AccordionDetails>
+          </Accordion>
+        ))
+      )}
     </Stack>
   )
 }
